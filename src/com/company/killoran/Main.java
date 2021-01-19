8bd8f8b582b85ecf26cbfcb7cf1666e9b1f02bf4
Mark <mark.killoran.jr@gmail.com>
2021-01-18 22:16:20 -0500

    first commit

---
+++ src/com/company/killoran/Main.java
@@ -0,0 +1,14 @@
+package com.company.killoran;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("test page Hellow World~!");
+        int a = 10;
+        int b = 50;
+        int c = a*b;
+        System.out.println(c);
+
+
+    }
+}
